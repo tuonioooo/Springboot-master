116265be9a1500a244b7748a2878773e71abe2d4
tuonioooo <daizhaoman@sina.com>
2018-07-23 17:16:59 +0800

    Spring boot 2.0.2整合log4j2多环境配置
    Spring boot 2.0.2整合log4j2多环境配置

---
+++ springboot-log4j-2-x-env/src/main/java/com/master/SpringbootLog4j2XEnvApplication.java
@@ -0,0 +1,12 @@
+package com.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLog4j2XEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLog4j2XEnvApplication.class, args);
+	}
+}
