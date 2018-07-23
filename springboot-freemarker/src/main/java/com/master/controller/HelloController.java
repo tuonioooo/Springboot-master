6cc175b26e76e30fede7e13b65ae88c039abb3be
tuonioooo <daizhaoman@sina.com>
2018-07-23 17:10:32 +0800

    Spring boot 2.0.2整合freemarker渲染视图
    Spring boot 2.0.2整合freemarker渲染视图

---
+++ springboot-freemarker/src/main/java/com/master/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.master.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author tony
+ * @version 1.0.0
+ *
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "https://blog.csdn.net/tuoni123");
+        return "index";
+    }
+
+}
