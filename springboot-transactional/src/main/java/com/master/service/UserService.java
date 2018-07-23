d1be7bdd423fa120d6c3219b8dc66563566d037b
tuonioooo <daizhaoman@sina.com>
2018-07-23 17:35:12 +0800

    Spring Boot 2.0.3中使用事务控制业务逻辑的完整性
    Spring Boot 2.0.3中使用事务控制业务逻辑的完整性

---
+++ springboot-transactional/src/main/java/com/master/service/UserService.java
@@ -0,0 +1,18 @@
+package com.master.service;
+
+
+import com.master.domain.User;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * Created by Administrator on 2016/5/27.
+ */
+public interface UserService {
+
+    @Transactional(isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED)
+    User login(String name, String password);
+
+}
