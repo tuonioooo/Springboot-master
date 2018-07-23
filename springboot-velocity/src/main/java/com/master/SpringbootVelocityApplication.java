fbd6db4c41456580e89dcd43f686caef8bd3aab1
tuonioooo <daizhaoman@sina.com>
2018-07-23 17:35:44 +0800

    Spring boot 1.4.2整合velocity渲染视图
    Spring boot 1.4.2整合velocity渲染视图

---
+++ springboot-velocity/src/main/java/com/master/SpringbootVelocityApplication.java
@@ -0,0 +1,12 @@
+package com.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVelocityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVelocityApplication.class, args);
+	}
+}
