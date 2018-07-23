c823c470865843d00d60be2a8cdb22cc1dbfce23
tuonioooo <daizhaoman@sina.com>
2018-07-23 17:19:23 +0800

    Spring boot 1.4.2实现邮件的模板发送，velocity Spring boot 1.4.2以后就不支持了
    Spring boot 1.4.2实现邮件的模板发送，velocity Spring boot 1.4.2以后就不支持了

---
+++ springboot-mail-1-4-2/src/main/java/com/master/SpringbootMail142Application.java
@@ -0,0 +1,12 @@
+package com.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMail142Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMail142Application.class, args);
+	}
+}
