d471e5fdde5d24b3688af9356a2022f42dec4311
tuonioooo <daizhaoman@sina.com>
2018-07-23 17:09:40 +0800

    flyway管理数据库版本
    flyway管理数据库版本

---
+++ springboot-flyway/src/main/java/com/master/service/UserService.java
@@ -0,0 +1,35 @@
+package com.master.service;
+
+/**
+ * @author tuonioooo
+ * @version 1.0.0
+ * @date 16/3/17 下午7:04.
+ * @blog https://blog.csdn.net/tuoni123
+ */
+public interface UserService {
+
+    /**
+     * 新增一个用户
+     * @param name
+     * @param age
+     */
+    void create(String name, Integer age);
+
+    /**
+     * 根据name删除一个用户高
+     * @param name
+     */
+    void deleteByName(String name);
+
+    /**
+     * 获取用户总量
+     */
+    Integer getAllUsers();
+
+    /**
+     * 删除所有用户
+     */
+    void deleteAllUsers();
+
+
+}
